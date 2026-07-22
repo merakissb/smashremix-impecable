b88d635f7ef946f1408257db93e46650095f5b1c
merakissb <mbustosquiroz@gmail.com>
2026-07-22 16:51:13 -0400

    add songs to impecable profile}

--- src/Toggles.asm
+++ src/Toggles.asm
@@ -2937,6 +2937,9 @@ scope Toggles {
         evaluate i({i} + 1)
     }
     add_to_impecable_music(ROCKSOLID)
+    add_to_impecable_music(CAMMY_STAGE)
+    add_to_impecable_music(KEN_STAGE)
+    add_to_impecable_music(TRUNKS_BATTLE)
 
     profiles:
     dw profile_defaults_CE
